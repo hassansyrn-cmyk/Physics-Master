3997946339595a68a5c69499391730cd0c5c9f40
hassansyrn-cmyk <hassan.syrn@gmail.com>
2026-07-31 01:09:30 +0400

    Update ProjectSetup.cs

--- Assets/Editor/ProjectSetup.cs
+++ Assets/Editor/ProjectSetup.cs
@@ -1,18 +1,92 @@
 #if UNITY_EDITOR
-using UnityEditor; using UnityEditor.SceneManagement; using UnityEngine; using PhysicsMaster.Core;
-public static class ProjectSetup {
-    public const string ScenePath="Assets/Scenes/Main.unity";
-    [InitializeOnLoadMethod] static void Auto(){EditorApplication.delayCall+=Ensure;}
-    public static void Ensure(){
-        if(!System.IO.File.Exists(ScenePath)){System.IO.Directory.CreateDirectory("Assets/Scenes");var s=EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects,NewSceneMode.Single);new GameObject("AppController").AddComponent<AppController>();EditorSceneManager.SaveScene(s,ScenePath);}
-        EditorBuildSettings.scenes=new[]{new EditorBuildSettingsScene(ScenePath,true)};
-        PlayerSettings.companyName="Afaq Games";PlayerSettings.productName="Physics Master: Draw & Solve";PlayerSettings.bundleVersion="0.2.0";
-        PlayerSettings.SetApplicationIdentifier(UnityEditor.Build.NamedBuildTarget.Android,"com.afaq.physicsmaster");
-        PlayerSettings.Android.bundleVersionCode=2;PlayerSettings.Android.minSdkVersion=AndroidSdkVersions.AndroidApiLevel26;
-        PlayerSettings.Android.targetSdkVersion=(AndroidSdkVersions)36;PlayerSettings.SetScriptingBackend(UnityEditor.Build.NamedBuildTarget.Android,ScriptingImplementation.IL2CPP);
-        PlayerSettings.Android.targetArchitectures=AndroidArchitecture.ARM64;PlayerSettings.defaultInterfaceOrientation=UIOrientation.Portrait;
-        PlayerSettings.SetApiCompatibilityLevel(UnityEditor.Build.NamedBuildTarget.Android,ApiCompatibilityLevel.NET_Standard_2_1);
+
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using PhysicsMaster.Core;
+
+public static class ProjectSetup
+{
+    public const string ScenePath = "Assets/Scenes/Main.unity";
+
+    [InitializeOnLoadMethod]
+    private static void AutoSetup()
+    {
+        EditorApplication.delayCall += Ensure;
+    }
+
+    public static void Ensure()
+    {
+        EnsureMainScene();
+        ConfigureBuildScenes();
+        ConfigurePlayerSettings();
+
         AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    private static void EnsureMainScene()
+    {
+        if (File.Exists(ScenePath))
+        {
+            return;
+        }
+
+        Directory.CreateDirectory("Assets/Scenes");
+
+        var scene = EditorSceneManager.NewScene(
+            NewSceneSetup.DefaultGameObjects,
+            NewSceneMode.Single);
+
+        var appControllerObject = new GameObject("AppController");
+        appControllerObject.AddComponent<AppController>();
+
+        EditorSceneManager.SaveScene(scene, ScenePath);
+    }
+
+    private static void ConfigureBuildScenes()
+    {
+        EditorBuildSettings.scenes = new[]
+        {
+            new EditorBuildSettingsScene(ScenePath, true)
+        };
+    }
+
+    private static void ConfigurePlayerSettings()
+    {
+        NamedBuildTarget androidTarget = NamedBuildTarget.Android;
+
+        PlayerSettings.companyName = "Afaq Games";
+        PlayerSettings.productName = "Physics Master: Draw & Solve";
+        PlayerSettings.bundleVersion = "0.2.0";
+
+        PlayerSettings.SetApplicationIdentifier(
+            androidTarget,
+            "com.afaq.physicsmaster");
+
+        PlayerSettings.Android.bundleVersionCode = 2;
+
+        PlayerSettings.Android.minSdkVersion =
+            AndroidSdkVersions.AndroidApiLevel26;
+
+        PlayerSettings.Android.targetSdkVersion =
+            (AndroidSdkVersions)36;
+
+        PlayerSettings.SetScriptingBackend(
+            androidTarget,
+            ScriptingImplementation.IL2CPP);
+
+        PlayerSettings.Android.targetArchitectures =
+            AndroidArchitecture.ARM64;
+
+        PlayerSettings.defaultInterfaceOrientation =
+            UIOrientation.Portrait;
+
+        PlayerSettings.SetApiCompatibilityLevel(
+            androidTarget,
+            ApiCompatibilityLevel.NET_Unity_4_8);
     }
 }
+
 #endif
